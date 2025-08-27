dd38c1e6ce905f4f3ebf2fa3f8bfc14f2bd3a259
Sunil Mishra <1839363+smishmash@users.noreply.github.com>
2025-08-26 18:28:32 -0700

    Histogram plotting using nivo
    Nivo doesn't have native histogram support, so I'm using a bar
    chart with the label the center of the bar range.

--- client/src/main/rgrr/client/app.cljs
+++ client/src/main/rgrr/client/app.cljs
@@ -2,45 +2,39 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs-http.client :as http]
             [cljs.core.async :as async]
+            ["@nivo/bar" :refer [Bar ResponsiveBar]]
             [reagent.core :as r]
             [reagent.dom.client :as rdc]))
 
 (def histogram-data (atom {}))
 
 (defn fetch-histogram []
-  (println "Pre-fetch")
   (go
     (let [resp (async/<! (http/get "/simulations/dummy/histograms"))]
-      (prn resp)
-      (reset! histogram-data (:body resp))))
-  ;; (http/get "/simulations/dummy/histograms"
-  ;;           {
-  ;;            ;;:handler #(reset! histogram-data (js->clj (.jsonParse (:body %))))
-  ;;            :handler (fn [resp] (println "In fetch") (println resp) (reset! histogram-data (:body resp)))
-  ;;            :error-handler #(js/console.error "Failed to fetch histogram" %)})
-  (println "Post-fetch")
-  (println (str @histogram-data)))
-
-(defn histogram-component []
-  [:div
-   [:h1 "Histogram"]
-   [:p (str @histogram-data)]]
-  )
+      (reset! histogram-data (:body resp)))))
 
 (defonce root (delay (rdc/create-root (js/document.getElementById "app"))))
 
 (defn render-histogram []
- (rdc/render @root [histogram-component]))
+  (rdc/render @root
+              [:div {:style {:width "100%" :height "500px"}}
+               [:>
+                ResponsiveBar
+                ;; Bar
+                {:data [{"country" "AD" "hotdogs" 60 "burgers" 80}
+                        {"country" "AE" "hotdogs" 30 "burgers" 50}
+                        {"country" "AF" "hotdogs" 70 "burgers" 90}]
+                 :keys ["hotdogs" "burgers"]
+                 :indexBy "country"
+                 :height 500
+                 :width 400
+                 :margin {:top 50 :right 50 :bottom 50 :left 60}
+                 :padding 0.3
+                 ;; :colors {:scheme "nivo"}
+                 ;; :axisBottom {:tickRotation 0}
+                 }]]))
 
 (add-watch histogram-data :redisplay render-histogram)
 
 (defn ^:export ^:dev/after-load init []
-  (fetch-histogram)
-  (render-histogram))
-
-(defn simple-component []
-  [:div
-   [:p "I am a component!"]
-   [:p.someclass
-    "I have " [:strong "bold"]
-    [:span {:style {:color "red"}} " and red "] "text."]])
+  (fetch-histogram))
