82c95221af7f3d59acef07dbdcd1121ef5fa9ec2
Sunil Mishra <1839363+smishmash@users.noreply.github.com>
2025-08-26 19:19:47 -0700

    Implement histogram navigation with next and prev buttons
    Clean up histogram display a bit further and improve code formatting.

--- client/src/main/rgrr/client/app.cljs
+++ client/src/main/rgrr/client/app.cljs
@@ -2,11 +2,13 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs-http.client :as http]
             [cljs.core.async :as async]
-            ["@nivo/bar" :refer [Bar ResponsiveBar]]
+            [clojure.pprint :as pp]
+            ["@nivo/bar" :refer [ResponsiveBar]]
             [reagent.core :as r]
             [reagent.dom.client :as rdc]))
 
 (def histogram-data (atom {}))
+(def histogram-index (atom 0))
 
 (defn fetch-histogram []
   (go
@@ -15,26 +17,39 @@
 
 (defonce root (delay (rdc/create-root (js/document.getElementById "app"))))
 
+(defn get-histogram-data [index]
+  (if (not (empty? @histogram-data))
+   (let [labels (map #(/ (+ %1 %2) 2)
+                     (:bin_edges @histogram-data)
+                     (rest (:bin_edges @histogram-data)))
+         data (nth (:epoch_distributions @histogram-data) index)]
+     (map (fn [l d] {"id" (pp/cl-format nil "~,2f" l) "density" d}) labels data))
+   []))
+
 (defn render-histogram []
   (rdc/render @root
-              [:div {:style {:width "100%" :height "500px"}}
-               [:>
-                ResponsiveBar
-                ;; Bar
-                {:data [{"country" "AD" "hotdogs" 60 "burgers" 80}
-                        {"country" "AE" "hotdogs" 30 "burgers" 50}
-                        {"country" "AF" "hotdogs" 70 "burgers" 90}]
-                 :keys ["hotdogs" "burgers"]
-                 :indexBy "country"
-                 :height 500
-                 :width 400
-                 :margin {:top 50 :right 50 :bottom 50 :left 60}
-                 :padding 0.3
-                 ;; :colors {:scheme "nivo"}
-                 ;; :axisBottom {:tickRotation 0}
-                 }]]))
+              [:div
+               [:div {:style {:width "100%" :height "500px"}}
+                [:> ResponsiveBar
+                 {:data (get-histogram-data @histogram-index)
+                  :keys ["density"]
+                  :height 500
+                  :width 400
+                  :margin {:top 50 :right 50 :bottom 50 :left 60}
+                  :padding 0.05
+                  :animate false
+                  :enableLabel false}]]
+               (let [max-index (- (count (:epoch_distributions @histogram-data)) 1)]
+                 [:div
+                  [:button {:disabled (or (empty? @histogram-data) (= @histogram-index 0))
+                            :onClick #(if (> @histogram-index 0) (swap! histogram-index dec))}
+                   "Previous"]
+                  [:button {:disabled (or (empty? @histogram-data) (= @histogram-index max-index))
+                            :onClick #(if (< @histogram-index max-index) (swap! histogram-index inc))}
+                   "Next"]])]))
 
 (add-watch histogram-data :redisplay render-histogram)
+(add-watch histogram-index :redisplay render-histogram)
 
 (defn ^:export ^:dev/after-load init []
   (fetch-histogram))
