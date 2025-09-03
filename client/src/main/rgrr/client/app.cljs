789deabbf9d1e94dd15ef05d10a101552266d10b
Sunil Mishra <1839363+smishmash@users.noreply.github.com>
2025-09-02 20:46:54 -0700

    Add styling and usability
    Some code has to be refactored to allow proper styling.
    Integrate shadow-css.

--- client/src/main/rgrr/client/app.cljs
+++ client/src/main/rgrr/client/app.cljs
@@ -5,7 +5,8 @@
             [clojure.pprint :as pp]
             ["@nivo/bar" :refer [ResponsiveBar]]
             [reagent.core :as r]
-            [reagent.dom.client :as rdc]))
+            [reagent.dom.client :as rdc]
+            [shadow.css]))
 
 (def histogram-data (atom {}))
 (def histogram-index (r/atom 0))
@@ -27,28 +28,49 @@
      (map (fn [l d] {"id" (pp/cl-format nil "~,2f" l) "density" d}) labels data))
    []))
 
+(defn render-header []
+  [:div {:style {:width "100%" :text-align "center"}}
+   [:h2 "Epoch " @histogram-index]])
+
+(defn render-chart []
+  [:div {:style {:width "100%" :height "500px" :border "1px solid black"}}
+   [:> ResponsiveBar
+    {:data @histogram-current
+     :keys ["density"]
+     :height 500
+     :width 400
+     :defaultHeight 500
+     :defaultWidth 400
+     :margin {:top 50 :right 50 :bottom 50 :left 60}
+     :padding 0.05
+     :enableLabel false}]])
+
+(defn render-controls []
+  (let [epoch-max (- (count (:epoch_distributions @histogram-data)) 1)]
+    [:div
+     [:button {:disabled (or (empty? @histogram-data) (= @histogram-index 0))
+               :onClick #(if (> @histogram-index 0) (swap! histogram-index dec))}
+      "Previous"]
+     [:input {:type "text"
+              :value @histogram-index
+              :onChange (fn [e]
+                          (let [new-epoch-str (-> e .-target .-value)
+                                new-epoch (when (and new-epoch-str (re-matches #"\d+" new-epoch-str))
+                                            (js/parseInt new-epoch-str 10))]
+                            ;; Ensure the input is a valid integer and in range before updating the state
+                            (when (and new-epoch (<= 0 new-epoch epoch-max))
+                              (reset! histogram-index new-epoch))))}]
+     [:button {:disabled (or (empty? @histogram-data) (= @histogram-index epoch-max))
+               :onClick #(if (< @histogram-index epoch-max) (swap! histogram-index inc))}
+      "Next"]]))
+
 (defn render-histogram []
-  (let [data (get-histogram-data @histogram-index)
-        epoch-max (- (count (:epoch_distributions @histogram-data)) 1)]
-    (rdc/render @root
-                [(fn []
-                   [:div
-                    [:div {:style {:width "100%" :height "500px"}}
-                     [:> ResponsiveBar
-                      {:data @histogram-current
-                       :keys ["density"]
-                       :height 500
-                       :width 400
-                       :margin {:top 50 :right 50 :bottom 50 :left 60}
-                       :padding 0.05
-                       :enableLabel false}]]
-                    [:div
-                     [:button {:disabled (or (empty? @histogram-data) (= @histogram-index 0))
-                               :onClick #(if (> @histogram-index 0) (swap! histogram-index dec))}
-                      "Previous"]
-                     [:button {:disabled (or (empty? @histogram-data) (= @histogram-index epoch-max))
-                               :onClick #(if (< @histogram-index epoch-max) (swap! histogram-index inc))}
-                      "Next"]]])])))
+  (rdc/render @root
+              [(fn []
+                 [:div
+                  [render-header]
+                  [render-chart]
+                  [render-controls]])]))
 
 (defn update-current []
   (reset! histogram-current (get-histogram-data @histogram-index)))
