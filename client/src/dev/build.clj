90b00f851246e6ce42f4d4cbff2275a9dd3f780e
Sunil Mishra <1839363+smishmash@users.noreply.github.com>
2025-10-07 21:54:14 -0700

    UI styling and usability improvements (#32)
    * Add styling and usability
    
    Some code has to be refactored to allow proper styling.
    Integrate shadow-css.
    
    * Set up building and loading UI stylesheet
    
    Minimal styling now works. Command to run server and watch client
    changes is:
    
    npx shadow-cljs run repl/start

---
+++ client/src/dev/build.clj
@@ -0,0 +1,18 @@
+(ns build
+  (:require
+    [shadow.css.build :as cb]
+    [clojure.java.io :as io]))
+
+(defn css-release [& args]
+  (let [build-state
+        (-> (cb/start)
+            (cb/index-path (io/file "src" "main") {})
+            (cb/generate
+              '{:ui
+                {:entries [rgrr.client.app]}})
+            (cb/minify)
+            (cb/write-outputs-to (io/file "public" "css")))]
+
+    (doseq [mod (vals (:chunks build-state))
+            {:keys [warning-type] :as warning} (:warnings mod)]
+      (prn [:CSS (name warning-type) (dissoc warning :warning-type)]))))
